e57cb56810ecf73141cb8736b3e78b92ee8d800c
Romain GOSSELIN <romaingosselin@mailo.com>
2022-03-24 11:50:26 +0100

    Restriction statut

--- STID.py
+++ STID.py
@@ -18,12 +18,13 @@ st.title("Critères", anchor = None)
 # Critères de recherche
 region = st.selectbox("Région 📍", df["Région"].unique())
 diplome = st.selectbox("Diplôme délivré 🏆", df["Diplôme délivré"].unique())
+statut = st.selectbox("Statut 💼", df["Statut"].unique())
 
 # Titre - Résultat
 st.title("Résultats", anchor = None)
 
 # Sélection des lignes selon le respect des critères
-df_restreint = df.loc[(df["Diplôme délivré"] == (diplome)) & (df["Région"] == (region))]
+df_restreint = df.loc[(df["Diplôme délivré"] == (diplome)) & (df["Région"] == (region)) & (df["Statut"] == (statut))]
 
 # Affichage du dataframe restreint
 st.dataframe(df_restreint)
@@ -48,32 +49,33 @@ st.download_button(label='✅ Télécharger le résultat', data = liste_formatio
 # Titre - Questionnaire
 st.title("Questionnaire", anchor = None)
 
-prenom = st.text_input("Indiquez votre prénom :")
-nom = st.text_input("Indiquez votre nom :")
-bac = st.radio("Quel Bac avez-vous fait ?",("Bac S (ou équivalent)", "Bac ES (ou équivalent)", "Bac L (ou équivalent)", "Bac STI2D", "Bac Professionnel", "Autre"))
-mention = st.radio("Quelle a été votre mention ? ",("Très Bien", "Bien", "Assez Bien", "Sans mention"))
-choix = st.text_input("Pourquoi avoir choisi STID et spécialement celui d'Aurillac ?")
-integration = st.radio("Avez-vous intégré STID :", ("Directement après le Bac", "1 an après le Bac", "2 ans après le Bac", "3 ans ou plus après le Bac"))
-stage = st.text_input("Dans quelle entreprise avez-vous effectué votre stage de fin de DUT ?")
-satisfaction_STID = st.select_slider("A quel point avez-vous apprécié le DUT STID", options = ["Pas du tout", "Pas vraiment", "Moyennement", "Plutôt", "Énormément"])
-    
-poursuite = st.radio("Que faites-vous actuellement ?", ("École d'ingénieur (en initial)", "École d'ingénieur (en alternance)", "Licence puis Master", "Licence professionelle", "Vie Active", "Autre"))
-if poursuite == "École d'ingénieur (en initial)" or poursuite == "École d'ingénieur (en alternance)" or poursuite == "Licence puis Master" or poursuite == "Licence professionelle":
-    statut = st.text_input("Quel est l'intitulé de votre formation ?")
-    ville = st.text_input("Dans quelle se déroule cette formation ?")
-    choix = st.radio("Avez-vous été accepté dans la formation que vous préfériez ?", ("Oui", "Non"))
-    satisfaction_formation = st.radio("Êtes-vous satisfait de votre formation actuelle ?", ("Oui", "Moyennement", "Non"))
-    metier = st.text_input("Quel métier voudriez-vous faire après vos études ?")
-elif poursuite == "Vie Active":
-    statut = st.text_input("Dans quelle entreprise travaillez-vous ?")
-    ville = st.text_input("Précisez la ville où vous travaillez")
-    metier = st.text_input("Quel poste occupez-vous ?")
-elif poursuite == "Autre":
-    statut = st.text_input("Précisez votre réponse")
+with st.container():
+    prenom = st.text_input("Indiquez votre prénom :")
+    nom = st.text_input("Indiquez votre nom :")
+    bac = st.radio("Quel Bac avez-vous fait ?",("Bac S (ou équivalent)", "Bac ES (ou équivalent)", "Bac L (ou équivalent)", "Bac STI2D", "Bac Professionnel", "Autre"))
+    mention = st.radio("Quelle a été votre mention ? ",("Très Bien", "Bien", "Assez Bien", "Sans mention"))
+    choix = st.text_input("Pourquoi avoir choisi STID et spécialement celui d'Aurillac ?")
+    integration = st.radio("Avez-vous intégré STID :", ("Directement après le Bac", "1 an après le Bac", "2 ans après le Bac", "3 ans ou plus après le Bac"))
+    stage = st.text_input("Dans quelle entreprise avez-vous effectué votre stage de fin de DUT ?")
+    satisfaction_STID = st.select_slider("A quel point avez-vous apprécié le DUT STID", options = ["Pas du tout", "Pas vraiment", "Moyennement", "Plutôt", "Énormément"])
+        
+    poursuite = st.radio("Que faites-vous actuellement ?", ("École d'ingénieur (en initial)", "École d'ingénieur (en alternance)", "Licence puis Master", "Licence professionelle", "Vie Active", "Autre"))
+    if poursuite == "École d'ingénieur (en initial)" or poursuite == "École d'ingénieur (en alternance)" or poursuite == "Licence puis Master" or poursuite == "Licence professionelle":
+        statut = st.text_input("Quel est l'intitulé de votre formation ?")
+        ville = st.text_input("Dans quelle se déroule cette formation ?")
+        choix = st.radio("Avez-vous été accepté dans la formation que vous préfériez ?", ("Oui", "Non"))
+        satisfaction_formation = st.radio("Êtes-vous satisfait de votre formation actuelle ?", ("Oui", "Moyennement", "Non"))
+        metier = st.text_input("Quel métier voudriez-vous faire après vos études ?")
+    elif poursuite == "Vie Active":
+        statut = st.text_input("Dans quelle entreprise travaillez-vous ?")
+        ville = st.text_input("Précisez la ville où vous travaillez")
+        metier = st.text_input("Quel poste occupez-vous ?")
+    elif poursuite == "Autre":
+        statut = st.text_input("Précisez votre réponse")
 
-recommandation = st.radio("Recommanderiez-vous STID à d'autres personnes ?", ("Oui", "Non"))
-if recommandation == "Non":
-    pourquoi = st.text_input("Pourquoi ?")
+    recommandation = st.radio("Recommanderiez-vous STID à d'autres personnes ?", ("Oui", "Non"))
+    if recommandation == "Non":
+        pourquoi = st.text_input("Pourquoi ?")
 
-contact = st.text_input("Donnez-nous un moyen de vous recontacter pour de futurs forums d'anciens étudiants par exemple")
-autorisation = st.checkbox("Autorisez-vous l'utilisation et le traitement de vos données personnelles dans le but d'une analyse statistique ? (Les données personnelles telles que les notes et noms seront bien sûr anonymisées)")
+    contact = st.text_input("Donnez-nous un moyen de vous recontacter pour de futurs forums d'anciens étudiants par exemple")
+    autorisation = st.checkbox("Autorisez-vous l'utilisation et le traitement de vos données personnelles dans le but d'une analyse statistique ? (Les données personnelles telles que les notes et noms seront bien sûr anonymisées)")
